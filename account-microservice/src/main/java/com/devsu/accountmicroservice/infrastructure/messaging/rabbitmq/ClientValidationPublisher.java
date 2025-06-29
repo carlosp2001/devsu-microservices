fc8a3597082404373e94f4ae015f93c8cbfac19e
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:37:55 -0600

    Add ClientValidationPublisher class for publishing client validation requests via RabbitMQ

---
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/messaging/rabbitmq/ClientValidationPublisher.java
@@ -0,0 +1,18 @@
+package com.devsu.accountmicroservice.infrastructure.messaging.rabbitmq;
+
+import com.devsu.accountmicroservice.application.port.out.ValidateClientUseCase;
+import com.devsu.library.infrastructure.messaging.rabbitmq.dto.enums.ClientValidationRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClientValidationPublisher implements ValidateClientUseCase {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void execute(ClientValidationRequestDTO request) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_CLIENT_VALIDATION, request);
+    }
+}
