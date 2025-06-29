6ae1000c9654a17f3487edceca9df6410ec200ed
carlos.pineda <pinedalberto@hotmail.com>
2025-06-28 21:03:56 -0600

    Add mapping methods for `UpdateClientResponseDTO` and `GetClientByIdResponseDTO` in `ClientRestMapper`

--- usermicroservice/src/main/java/com/devsu/usermicroservice/infrastructure/rest/mapper/ClientRestMapper.java
+++ usermicroservice/src/main/java/com/devsu/usermicroservice/infrastructure/rest/mapper/ClientRestMapper.java
@@ -3,6 +3,8 @@ package com.devsu.usermicroservice.infrastructure.rest.mapper;
 import com.devsu.usermicroservice.domain.model.Cliente;
 import com.devsu.usermicroservice.domain.model.enums.PersonaGenero;
 import com.devsu.usermicroservice.infrastructure.rest.dto.CreateClientResponseDTO;
+import com.devsu.usermicroservice.infrastructure.rest.dto.GetClientByIdResponseDTO;
+import com.devsu.usermicroservice.infrastructure.rest.dto.UpdateClientResponseDTO;
 import com.devsu.usermicroservice.infrastructure.rest.dto.enums.PersonaGeneroDTO;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,36 @@ public class ClientRestMapper {
                 domain.getPassword(),
                 domain.getCreatedAt(),
                 domain.getCreatedAt()
+        );
+    }
+
+    public UpdateClientResponseDTO toUpdateDto(Cliente domain) {
+        return new UpdateClientResponseDTO(
+                domain.getId(),
+                domain.getNombre(),
+                domain.getEdad(),
+                toDto(domain.getGenero()),
+                domain.getDireccion(),
+                domain.getTelefono(),
+                domain.getEstado(),
+                domain.getPassword(),
+                domain.getCreatedAt(),
+                domain.getUpdatedAt()
+        );
+    }
+
+    public GetClientByIdResponseDTO toGetResponseDto(Cliente domain) {
+        return new GetClientByIdResponseDTO(
+                domain.getId(),
+                domain.getNombre(),
+                domain.getEdad(),
+                toDto(domain.getGenero()),
+                domain.getDireccion(),
+                domain.getTelefono(),
+                domain.getEstado(),
+                domain.getPassword(),
+                domain.getCreatedAt(),
+                domain.getUpdatedAt()
         );
     }
 
