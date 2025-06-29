cd8a8f6366637a60fd34195b5afbb85d55ff38bb
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:40:26 -0600

    Refactor ClientValidationRequestDTO package structure and import CuentaTipoDTO

--- library/src/main/java/com/devsu/library/infrastructure/messaging/rabbitmq/dto/ClientValidationRequestDTO.java
+++ library/src/main/java/com/devsu/library/infrastructure/messaging/rabbitmq/dto/ClientValidationRequestDTO.java
@@ -1,6 +1,7 @@
-package com.devsu.library.infrastructure.messaging.rabbitmq.dto.enums;
+package com.devsu.library.infrastructure.messaging.rabbitmq.dto;
 
 import com.devsu.library.domain.model.Peticion;
+import com.devsu.library.infrastructure.messaging.rabbitmq.dto.enums.CuentaTipoDTO;
 
 import java.io.Serializable;
 
