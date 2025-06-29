f6ee116d7d9728486d8055e7e6f5e11809611fab
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 17:08:12 -0600

    Add handlers for InsufficientBalance and TransactionNotFound exceptions in GlobalExceptionHandler

--- account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/rest/exception/GlobalExceptionHandler.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/rest/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.devsu.accountmicroservice.infrastructure.rest.exception;
 
 import com.devsu.accountmicroservice.domain.exception.AccountNotFoundException;
+import com.devsu.accountmicroservice.domain.exception.InsufficientBalanceException;
+import com.devsu.accountmicroservice.domain.exception.TransactionNotFoundException;
 import com.devsu.library.domain.exception.PeticionNotFoundException;
 import com.devsu.library.infrastructure.rest.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -27,6 +29,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAccountNotFound(AccountNotFoundException exception) {
         return buildResponse(HttpStatus.NOT_FOUND, "AccountNotFound", exception.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ErrorResponse> handleBalanceException(InsufficientBalanceException exception) {
+        return buildResponse(HttpStatus.BAD_REQUEST, "InsufficientBalance", exception.getMessage());
+    }
+
+    @ExceptionHandler(TransactionNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTransactionNotFoundException(TransactionNotFoundException exception) {
+        return buildResponse(HttpStatus.NOT_FOUND, "TransactionNotFound", exception.getMessage());
     }
 
     private ResponseEntity<ErrorResponse> buildResponse(HttpStatus status, String type, String message) {
