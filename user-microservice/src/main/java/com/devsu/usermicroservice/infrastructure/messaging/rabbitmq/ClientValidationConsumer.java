3a76de71a6f68419323f15d853c229bd72450b85
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:34:01 -0600

    Add `ClientValidationConsumer` to handle client validation requests via RabbitMQ

---
+++ user-microservice/src/main/java/com/devsu/usermicroservice/infrastructure/messaging/rabbitmq/ClientValidationConsumer.java
@@ -0,0 +1,40 @@
+package com.devsu.usermicroservice.infrastructure.messaging.rabbitmq;
+
+import com.devsu.usermicroservice.application.port.in.ClientValidationUseCase;
+import com.devsu.usermicroservice.infrastructure.persistence.repository.ClientRepository;
+import com.devsu.library.domain.model.enums.PeticionEstado;
+import com.devsu.library.infrastructure.messaging.rabbitmq.dto.enums.ClientValidationRequestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ClientValidationConsumer implements ClientValidationUseCase {
+    private final ClientRepository clientRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_CLIENT_VALIDATION)
+    @Override
+    public void execute(ClientValidationRequestDTO request) {
+        log.info("Recibida la petición de validación del cliente: {}", request.clientId());
+        if (clientRepository.existsById(request.clientId())) {
+            log.info("Cliente con ID {} existe. Procediendo a validar.", request.clientId());
+            rabbitTemplate.convertAndSend(
+                    RabbitMQConfig.EXCHANGE_NAME,
+                    RabbitMQConfig.ROUTING_KEY_ACCOUNT_CREATION,
+                    request
+            );
+        } else {
+            rabbitTemplate.convertAndSend(
+                    RabbitMQConfig.EXCHANGE_NAME,
+                    RabbitMQConfig.ROUTING_KEY_CLIENT_VALIDATION_FAILED,
+                    request.request()
+            );
+            log.warn("Cliente con ID {} no existe. No se puede validar.", request.clientId());
+        }
+    }
+}
