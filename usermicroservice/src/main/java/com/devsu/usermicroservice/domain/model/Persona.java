14baefde0520841d13514a80baf0f4be5a345b9f
carlos.pineda <pinedalberto@hotmail.com>
2025-06-28 21:05:02 -0600

    Add `@Getter` and `@Setter` annotations to `Persona` fields and refactor constructors and methods

--- usermicroservice/src/main/java/com/devsu/usermicroservice/domain/model/Persona.java
+++ usermicroservice/src/main/java/com/devsu/usermicroservice/domain/model/Persona.java
@@ -2,57 +2,44 @@ package com.devsu.usermicroservice.domain.model;
 
 
 import com.devsu.usermicroservice.domain.model.enums.PersonaGenero;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 public class Persona {
 
     private final String id;
+    @Getter
+    @Setter
     private String nombre;
+    @Getter
+    @Setter
     private Short edad;
+    @Getter
+    @Setter
     private PersonaGenero genero;
+    @Getter
+    @Setter
     private String direccion;
-    private String identificacion;
+
+    @Getter
+    @Setter
     private String telefono;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Persona(String id, String nombre, short edad, PersonaGenero genero, String direccion, String identificacion, String telefono) {
+    public Persona(String id, String nombre, short edad, PersonaGenero genero, String direccion, String telefono) {
         this.id = id;
         this.nombre = nombre;
         this.edad = edad;
         this.genero = genero;
         this.direccion = direccion;
-        this.identificacion = identificacion;
         this.telefono = telefono;
     }
 
     public String getPersonId() {
         return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public Short getEdad() {
-        return edad;
-    }
-
-    public PersonaGenero getGenero() {
-        return genero;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public String getIdentificacion() {
-        return identificacion;
-    }
-
-    public String getTelefono() {
-        return telefono;
     }
 
     public LocalDateTime getPersonCreatedAt() {
@@ -61,5 +48,32 @@ public class Persona {
 
     public LocalDateTime getPersonUpdatedAt() {
         return updatedAt;
+    }
+
+    protected Persona(String id,
+                      String nombre,
+                      Short edad,
+                      PersonaGenero genero,
+                      String direccion,
+                      String telefono,
+                      LocalDateTime createdAt,
+                      LocalDateTime updatedAt) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    protected void updatePerson(String nombre, Short edad, PersonaGenero genero, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.updatedAt = LocalDateTime.now();
     }
 }
