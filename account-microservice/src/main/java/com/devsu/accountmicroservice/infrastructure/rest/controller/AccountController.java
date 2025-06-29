af8a88b68a496ef77dbfae4aaf972413919a15d1
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 14:18:20 -0600

    Add getAccountById method to AccountController for account retrieval

--- account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/rest/controller/AccountController.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/rest/controller/AccountController.java
@@ -2,11 +2,11 @@ package com.devsu.accountmicroservice.infrastructure.rest.controller;
 
 import com.devsu.accountmicroservice.application.command.CreateAccountCommand;
 import com.devsu.accountmicroservice.application.port.in.CreateAccountUseCase;
+import com.devsu.accountmicroservice.application.port.in.GetAccountByIdUseCase;
 import com.devsu.accountmicroservice.application.port.in.GetPeticionStatusUseCase;
-import com.devsu.accountmicroservice.application.service.GetPeticionStatusService;
 import com.devsu.accountmicroservice.infrastructure.rest.dto.CreateAccountRequestDTO;
+import com.devsu.accountmicroservice.infrastructure.rest.dto.GetAccountByIdResponseDTO;
 import com.devsu.library.domain.model.Peticion;
-import com.devsu.library.domain.model.enums.PeticionEstado;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +20,7 @@ import java.util.UUID;
 public class AccountController {
     private final CreateAccountUseCase createAccountUseCase;
     private final GetPeticionStatusUseCase getPeticionStatusUseCase;
+    private final GetAccountByIdUseCase getAccountByIdUseCase;
 
     @PostMapping
     public ResponseEntity<Peticion> createAccount(@RequestBody @Valid CreateAccountRequestDTO requestDTO) {
@@ -33,8 +34,12 @@ public class AccountController {
 
     @GetMapping("/{peticionId}/estado")
     public ResponseEntity<Peticion> getPeticionEstado(@PathVariable("peticionId")
-                                                            UUID peticionId) {
+                                                      UUID peticionId) {
         return ResponseEntity.ok(getPeticionStatusUseCase.execute(peticionId));
     }
 
+    @GetMapping("/{accountId}")
+    public ResponseEntity<GetAccountByIdResponseDTO> getAccountById(@PathVariable("accountId") String accountId) {
+        return ResponseEntity.ok(getAccountByIdUseCase.execute(accountId));
+    }
 }
