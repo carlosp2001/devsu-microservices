393abc19c163c3cf01d425b6297aaf3c5047ea48
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 13:50:11 -0600

    Remove AccountCreationUseCase interface and related dependencies

--- account-microservice/src/main/java/com/devsu/accountmicroservice/application/port/in/AccountCreationUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.devsu.accountmicroservice.application.port.in;
-
-import com.devsu.accountmicroservice.application.command.CreateAccountCommand;
-import com.devsu.library.domain.model.Peticion;
-
-public interface AccountCreationUseCase {
-    Peticion execute(CreateAccountCommand command);
-}
