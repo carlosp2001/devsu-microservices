acaf481cde0ea21af615038d849ea77608894b00
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:34:15 -0600

    Add `AccountCreationPublisher` to handle account creation messages via RabbitMQ

---
+++ user-microservice/src/main/java/com/devsu/usermicroservice/infrastructure/messaging/rabbitmq/AccountCreationPublisher.java
@@ -0,0 +1,18 @@
+package com.devsu.usermicroservice.infrastructure.messaging.rabbitmq;
+
+import com.devsu.usermicroservice.application.port.out.AccountCreationUseCase;
+import com.devsu.library.infrastructure.messaging.rabbitmq.dto.enums.ClientValidationRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccountCreationPublisher implements AccountCreationUseCase {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void execute(ClientValidationRequestDTO request) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_ACCOUNT_CREATION, request);
+    }
+}
