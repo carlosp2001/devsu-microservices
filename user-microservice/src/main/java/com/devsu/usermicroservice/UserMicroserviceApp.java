c1f970f8f29ba2bec8457c966a4c59f447252c0c
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:29:44 -0600

    Add EntityScan configuration for user and library entities

--- user-microservice/src/main/java/com/devsu/usermicroservice/UserMicroserviceApp.java
+++ user-microservice/src/main/java/com/devsu/usermicroservice/UserMicroserviceApp.java
@@ -2,8 +2,13 @@ package com.devsu.usermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "com.devsu.usermicroservice.infrastructure.persistence.entity",
+        "com.devsu.library.infrastructure.persistence.entity"
+})
 public class UserMicroserviceApp {
 
     public static void main(String[] args) {
