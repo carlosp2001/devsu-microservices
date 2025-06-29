f8cd2a1b6fecbbe27b8ce0713b1aa75fbc552f61
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 17:07:48 -0600

    Add MovimientoEntity for movimiento persistence mapping

---
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/persistence/entity/MovimientoEntity.java
@@ -0,0 +1,34 @@
+package com.devsu.accountmicroservice.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movimiento")
+@Setter
+@Getter
+public class MovimientoEntity {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String tipo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cuenta_id", nullable = false)
+    private CuentaEntity cuentaEntity;
+
+    @Column(nullable = false)
+    private Double monto;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
