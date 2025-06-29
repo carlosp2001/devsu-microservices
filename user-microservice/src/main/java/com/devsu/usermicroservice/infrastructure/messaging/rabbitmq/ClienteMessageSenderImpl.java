12c705bb6571d0c8fe61206b790742457321d99f
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:34:09 -0600

    Reintroduce `ClienteMessageSenderImpl` in `user-microservice` module as RabbitMQ message sender implementation

---
+++ user-microservice/src/main/java/com/devsu/usermicroservice/infrastructure/messaging/rabbitmq/ClienteMessageSenderImpl.java
@@ -0,0 +1,20 @@
+package com.devsu.usermicroservice.infrastructure.messaging.rabbitmq;
+
+import com.devsu.usermicroservice.application.port.out.ClienteMessageSender;
+import com.devsu.usermicroservice.domain.model.Cliente;
+import com.devsu.library.domain.model.Peticion;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClienteMessageSenderImpl implements ClienteMessageSender {
+
+    private final AmqpTemplate amqpTemplate;
+
+    @Override
+    public void sendUserCreationMessage(Cliente client, Peticion peticion) {
+
+    }
+}
