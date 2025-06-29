cf69e39507e3634126f96c136e0f0644c412c768
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 14:35:12 -0600

    Add DeleteAccountService implementation for account deletion functionality

---
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/application/service/DeleteAccountService.java
@@ -0,0 +1,22 @@
+package com.devsu.accountmicroservice.application.service;
+
+import com.devsu.accountmicroservice.application.port.in.DeleteAccountUseCase;
+import com.devsu.accountmicroservice.domain.exception.AccountNotFoundException;
+import com.devsu.accountmicroservice.infrastructure.persistence.repository.CuentaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteAccountService implements DeleteAccountUseCase {
+    private final CuentaRepository cuentaRepository;
+
+    @Override
+    public void execute(String accountId) {
+        if (cuentaRepository.existsById(accountId)) {
+            cuentaRepository.deleteById(accountId);
+        } else {
+            throw new AccountNotFoundException("Cuenta no encontrada con ID: " + accountId);
+        }
+    }
+}
