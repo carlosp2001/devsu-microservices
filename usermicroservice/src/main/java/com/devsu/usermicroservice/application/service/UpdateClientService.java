9c205121394954dc26dbff6e0ed713ea10629ff9
carlos.pineda <pinedalberto@hotmail.com>
2025-06-28 21:06:03 -0600

    Add UpdateClientService implementation for updating client details

---
+++ usermicroservice/src/main/java/com/devsu/usermicroservice/application/service/UpdateClientService.java
@@ -0,0 +1,43 @@
+package com.devsu.usermicroservice.application.service;
+
+import com.devsu.usermicroservice.application.command.UpdateClientCommand;
+import com.devsu.usermicroservice.application.port.in.UpdateClientUseCase;
+import com.devsu.usermicroservice.domain.exception.ClientNotFoundException;
+import com.devsu.usermicroservice.domain.model.Cliente;
+import com.devsu.usermicroservice.domain.model.enums.PersonaGenero;
+import com.devsu.usermicroservice.infrastructure.mapper.ClienteMapper;
+import com.devsu.usermicroservice.infrastructure.persistence.entity.ClienteEntity;
+import com.devsu.usermicroservice.infrastructure.persistence.repository.ClienteRepository;
+import com.devsu.usermicroservice.infrastructure.rest.dto.UpdateClientResponseDTO;
+import com.devsu.usermicroservice.infrastructure.rest.mapper.ClientRestMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateClientService implements UpdateClientUseCase {
+    private final ClienteRepository clientRepository;
+    private final ClienteMapper clienteMapper;
+    private final ClientRestMapper clientRestMapper;
+
+    @Override
+    public UpdateClientResponseDTO execute(UpdateClientCommand command) {
+        ClienteEntity clientEntity = clientRepository.findById(command.id())
+                .orElseThrow(() ->
+                        new ClientNotFoundException("Cliente no encontrado con ID: " + command.id()));
+
+        Cliente clienteDomain = clienteMapper.toDomain(clientEntity);
+        clienteDomain.update(
+                command.nombre(),
+                command.edad(),
+                PersonaGenero.valueOf(command.genero().name()),
+                command.direccion(),
+                command.telefono(),
+                command.estado(),
+                command.password()
+        );
+
+        clientRepository.save(clienteMapper.toEntity(clienteDomain));
+        return clientRestMapper.toUpdateDto(clienteDomain);
+    }
+}
