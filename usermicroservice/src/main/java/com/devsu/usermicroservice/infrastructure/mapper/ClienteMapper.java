bb0805fca914ac403932234c08e394a6bae1cf71
carlos.pineda <pinedalberto@hotmail.com>
2025-06-28 21:03:11 -0600

    Add `toDomain` method in `ClienteMapper` to map `ClienteEntity` to `Cliente` domain model

--- usermicroservice/src/main/java/com/devsu/usermicroservice/infrastructure/mapper/ClienteMapper.java
+++ usermicroservice/src/main/java/com/devsu/usermicroservice/infrastructure/mapper/ClienteMapper.java
@@ -1,5 +1,7 @@
 package com.devsu.usermicroservice.infrastructure.mapper;
 
+import com.devsu.usermicroservice.domain.model.Cliente;
+import com.devsu.usermicroservice.domain.model.enums.PersonaGenero;
 import com.devsu.usermicroservice.infrastructure.persistence.entity.ClienteEntity;
 import com.devsu.usermicroservice.infrastructure.persistence.entity.PersonaEntity;
 import org.springframework.stereotype.Component;
@@ -26,5 +28,25 @@ public class ClienteMapper {
         clientEntity.setUpdatedAt(client.getUpdatedAt());
 
         return clientEntity;
+    }
+
+    public Cliente toDomain(ClienteEntity clientEntity) {
+        Cliente client = Cliente.hydrate(
+                clientEntity.getId(),
+                clientEntity.getPersonaEntity().getId(),
+                clientEntity.getPersonaEntity().getNombre(),
+                clientEntity.getPersonaEntity().getEdad(),
+                PersonaGenero.valueOf(clientEntity.getPersonaEntity().getGenero()),
+                clientEntity.getPersonaEntity().getDireccion(),
+                clientEntity.getPersonaEntity().getTelefono(),
+                clientEntity.getEstado(),
+                clientEntity.getPassword(),
+                clientEntity.getPersonaEntity().getCreatedAt(),
+                clientEntity.getPersonaEntity().getUpdatedAt(),
+                clientEntity.getCreatedAt(),
+                clientEntity.getUpdatedAt()
+        );
+
+        return client;
     }
 }
