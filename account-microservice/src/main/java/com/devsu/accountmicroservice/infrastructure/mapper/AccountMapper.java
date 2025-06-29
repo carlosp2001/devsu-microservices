e4c869e70b6973895cc2f4b960111e1b1128afff
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 14:18:09 -0600

    Refactor AccountMapper to use updated saldo field and add toDomain method for CuentaEntity conversion

--- account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/mapper/AccountMapper.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/mapper/AccountMapper.java
@@ -2,6 +2,7 @@ package com.devsu.accountmicroservice.infrastructure.mapper;
 
 
 import com.devsu.accountmicroservice.domain.model.Cuenta;
+import com.devsu.accountmicroservice.domain.model.enums.CuentaTipo;
 import com.devsu.accountmicroservice.infrastructure.persistence.entity.CuentaEntity;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,24 @@ public class AccountMapper {
     public CuentaEntity toEntity(Cuenta domain) {
         CuentaEntity entity = new CuentaEntity();
         entity.setId(domain.getId());
-        entity.setSaldoInicial(domain.getSaldoInicial());
+        entity.setSaldo(domain.getSaldo());
         entity.setTipo(domain.getTipo().name());
         entity.setEstado(domain.getEstado());
         entity.setClienteId(domain.getClienteId());
         entity.setCreatedAt(domain.getCreatedAt());
         entity.setUpdatedAt(domain.getUpdatedAt());
         return entity;
+    }
+
+    public Cuenta toDomain(CuentaEntity entity) {
+        return Cuenta.hydrate(
+                entity.getId(),
+                entity.getSaldo(),
+                CuentaTipo.valueOf(entity.getTipo()),
+                entity.getEstado(),
+                entity.getClienteId(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
     }
 }
