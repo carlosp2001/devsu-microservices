57e14ff42db2efd377a3556ee311e2e32f72d216
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 11:35:23 -0600

    Add SQL script for database schema creation and UUID extension

---
+++ account-microservice/src/main/resources/db/migration/V1_1_0__base_datos.sql
@@ -0,0 +1,36 @@
+create extension if not exists "uuid-ossp";
+
+create sequence cuenta_id_seq start 1;
+
+-- Cuenta
+create function generate_cuenta_id()
+    returns text as
+$$
+declare
+    seq_num int;
+begin
+    seq_num := nextval('cuenta_id_seq');
+    return 'CUENTA-' || lpad(seq_num::text, 5, '0');
+end;
+$$ language plpgsql;
+
+create table cuenta
+(
+    id            text         not null primary key,
+    saldo_inicial decimal      not null,
+    tipo          varchar(255) not null,
+    estado        boolean      not null,
+    cliente_id    text         not null,
+    created_at    timestamp    not null default now(),
+    updated_at    timestamp    not null default now()
+);
+
+-- Cliente
+create table peticion
+(
+    peticion_id uuid                  default uuid_generate_v4() not null primary key,
+    estado      varchar(255) not null,
+    mensaje     varchar(255) not null,
+    created_at  timestamp    not null default now(),
+    updated_at  timestamp    not null default now()
+)
