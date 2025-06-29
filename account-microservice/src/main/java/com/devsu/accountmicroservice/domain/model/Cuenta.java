c980db466d9f59c8618009de300d239f471e6820
carlos.pineda <pinedalberto@hotmail.com>
2025-06-29 14:17:37 -0600

    Rename `saldoInicial` to `saldo` and add static `hydrate` constructor in `Cuenta` model.

--- account-microservice/src/main/java/com/devsu/accountmicroservice/domain/model/Cuenta.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/domain/model/Cuenta.java
@@ -8,20 +8,34 @@ import java.time.LocalDateTime;
 @Getter
 public class Cuenta {
     private String id;
-    private Double saldoInicial;
+    private Double saldo;
     private CuentaTipo tipo;
     private Boolean estado;
     private String clienteId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Cuenta(String id, Double saldoInicial, CuentaTipo tipo, String clienteId) {
+    public Cuenta(String id, Double saldoInical, CuentaTipo tipo, String clienteId) {
         this.id = id;
-        this.saldoInicial = saldoInicial;
+        this.saldo = saldoInical;
         this.tipo = tipo;
         this.estado = true;
         this.clienteId = clienteId;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
+    }
+
+    private Cuenta(String id, Double saldo, CuentaTipo tipo, Boolean estado, String clienteId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.saldo = saldo;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.clienteId = clienteId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static Cuenta hydrate(String id, Double saldo, CuentaTipo tipo, Boolean estado, String clienteId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        return new Cuenta(id, saldo, tipo, estado, clienteId, createdAt, updatedAt);
     }
 }
