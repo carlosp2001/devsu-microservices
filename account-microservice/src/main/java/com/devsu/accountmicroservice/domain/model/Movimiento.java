2644504a55313e64a045c291139f1fa421ad8f31
carlos.pineda <pinedalberto@hotmail.com>
2025-06-30 00:03:53 -0600

    Add `saldoInicial` and `saldoDisponible` fields to `Movimiento` model and update constructors accordingly

--- account-microservice/src/main/java/com/devsu/accountmicroservice/domain/model/Movimiento.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/domain/model/Movimiento.java
@@ -11,28 +11,34 @@ public class Movimiento {
     private MovimientoTipo tipo;
     private String cuentaId;
     private Double monto;
+    private Double saldoInicial;
+    private Double saldoDisponible;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Movimiento(String id, MovimientoTipo tipo, Double monto, String cuentaId) {
+    public Movimiento(String id, MovimientoTipo tipo, Double monto, String cuentaId, Double saldoInicial, Double saldoDisponible) {
         this.id = id;
         this.tipo = tipo;
         this.monto = monto;
         this.cuentaId = cuentaId;
+        this.saldoInicial = saldoInicial;
+        this.saldoDisponible = saldoDisponible;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
 
-    private Movimiento(String id, MovimientoTipo tipo, String cuentaId, Double monto, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    private Movimiento(String id, MovimientoTipo tipo, String cuentaId, Double monto, LocalDateTime createdAt, LocalDateTime updatedAt, Double saldoInicial, Double saldoDisponible) {
         this.id = id;
         this.tipo = tipo;
         this.cuentaId = cuentaId;
         this.monto = monto;
+        this.saldoInicial = saldoInicial;
+        this.saldoDisponible = saldoDisponible;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
 
-    public static Movimiento hydrate(String id, MovimientoTipo tipo, String cuentaId, Double monto, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        return new Movimiento(id, tipo, cuentaId, monto, createdAt, updatedAt);
+    public static Movimiento hydrate(String id, MovimientoTipo tipo, String cuentaId, Double monto, LocalDateTime createdAt, LocalDateTime updatedAt, Double saldoInicial, Double saldoDisponible) {
+        return new Movimiento(id, tipo, cuentaId, monto, createdAt, updatedAt, saldoInicial, saldoDisponible);
     }
 }
