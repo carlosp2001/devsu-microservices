a3ea7cf8064bf219f9957b5361216b750068a75c
carlos.pineda <pinedalberto@hotmail.com>
2025-06-30 00:03:36 -0600

    Add query to fetch movimientos by client ID and date range with pagination

--- account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/persistence/repository/MovimientoRepository.java
+++ account-microservice/src/main/java/com/devsu/accountmicroservice/infrastructure/persistence/repository/MovimientoRepository.java
@@ -1,9 +1,26 @@
 package com.devsu.accountmicroservice.infrastructure.persistence.repository;
 
 import com.devsu.accountmicroservice.infrastructure.persistence.entity.MovimientoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface MovimientoRepository extends JpaRepository<MovimientoEntity, String> {
+    @Query("""
+        SELECT m FROM MovimientoEntity m
+        WHERE m.cuentaEntity.clienteId = :clientId
+          AND m.createdAt BETWEEN :startDate AND :endDate
+    """)
+    Page<MovimientoEntity> findAllByClientAndDateBetween(
+            @Param("clientId") String clientId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
 }
